f326870cf1506eedfc66e44a12f50d87fd11ff97
Sergey Sherbakov <sherbakov990908@yandex.ru>
2025-03-08 17:05:46 +0300

    Create Task Management System template

---
+++ task-management-system/src/test/java/effective/mobile/task_management_system/TaskManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package effective.mobile.task_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
